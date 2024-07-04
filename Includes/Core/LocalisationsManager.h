bf3584af88bae15864d5b3c41e77fea499ad4c03
fedjoyd5 <fedjoyd5.fedmail@gmail.com>
2024-07-04 17:32:40 +0200

    [Dev] Add Localisation data save Add Localisation editor control

--- Includes/Core/LocalisationsManager.h
+++ Includes/Core/LocalisationsManager.h
@@ -14,14 +14,15 @@ namespace Core
 	class LocalisationsManager
 	{
 	public:
-		LocalisationsManager() : m_currentLanguage(0u), m_path(DEFAULT_LOCATION_PATH) { m_language.push_back({"english", "English"}); m_language.push_back({"french", "Francais"}); }
+		LocalisationsManager() : m_currentLanguage(0u), m_path(DEFAULT_LOCATION_PATH) { m_languages.push_back({"english", "English"}); }
 		~LocalisationsManager() {}
 
 		void SetLocalisationFilePath(const std::string& p_newPath) { m_path = p_newPath; }
-		void LoadLanguageList(tinyxml2::XMLElement* p_LanguageList);
-		const std::vector<std::pair<std::string, std::string>>& GetLanguage() { return m_language; }
+		void LoadLanguagesList(tinyxml2::XMLElement* p_LanguageList);
+		const std::vector<std::pair<std::string, std::string>>& GetLanguages() { return m_languages; }
 		
 		bool SetLanguage(size_t p_nextLanguage, bool p_forceLoad = false);
+		size_t GetLanguage() { return m_currentLanguage; }
 
 		static const std::string& GetLocalFormat(const std::string& p_Identifier);
 
@@ -34,7 +35,7 @@ namespace Core
 
 	private:
 		size_t m_currentLanguage;
-		std::vector<std::pair<std::string, std::string>> m_language;
+		std::vector<std::pair<std::string, std::string>> m_languages;
 		std::unordered_map<std::string, std::string> m_data;
 
 		std::string m_path;
--- Sources/Core/LocalisationsManager.cpp
+++ Sources/Core/LocalisationsManager.cpp
@@ -11,13 +11,13 @@ void Core::LocalisationsManager::LoadLanguageList(tinyxml2::XMLElement* p_Langua
 
 bool Core::LocalisationsManager::SetLanguage(size_t p_nextLanguage, bool p_forceLoad)
 {
-	if ((m_currentLanguage == p_nextLanguage && !p_forceLoad) || p_nextLanguage >= m_language.size())
+	if ((m_currentLanguage == p_nextLanguage && !p_forceLoad) || p_nextLanguage >= m_languages.size())
 		return false;
 
 	m_currentLanguage = p_nextLanguage;
 
 	tinyxml2::XMLDocument m_LangXMLData;
-	if (m_LangXMLData.LoadFile((m_path + m_language[m_currentLanguage].first + ".xml").c_str()) == tinyxml2::XML_SUCCESS)
+	if (m_LangXMLData.LoadFile((m_path + m_languages[m_currentLanguage].first + ".xml").c_str()) == tinyxml2::XML_SUCCESS)
 	{
 		m_data.clear();
 
@@ -39,7 +39,7 @@ bool Core::LocalisationsManager::SetLanguage(size_t p_nextLanguage, bool p_force
 		}
 	}
 
-	R3DE_INFO("Game's language set to %s", m_language[m_currentLanguage].first.c_str());
+	R3DE_INFO("Game's language set to %s", m_languages[m_currentLanguage].first.c_str());
 
 	return true;
 }
@@ -58,15 +58,145 @@ const std::string& Core::LocalisationsManager::GetLocalFormat(const std::string&
 }
 
 #ifdef _EDITOR
+#include "imgui.h"
+#include "imgui_stdlib.h"
+
 void Core::LocalisationsManager::SaveLanguageList(tinyxml2::XMLElement* p_LanguageList)
 {
 }
 
 void Core::LocalisationsManager::SaveCurrentLanguage()
 {
+	if (m_currentLanguage >= m_languages.size())
+		return;
+
+	tinyxml2::XMLDocument currentLanguageXMLDocument;
+	currentLanguageXMLDocument.Parse("<?xml version=\"1.0\"?><Datas></Datas>");
+
+	tinyxml2::XMLElement* listXMLDatas = currentLanguageXMLDocument.FirstChildElement("Datas");
+	tinyxml2::XMLElement* currentXMLData = nullptr;
+
+	for (std::unordered_map<std::string, std::string>::const_iterator ConstLangIterator = m_data.begin(); ConstLangIterator != m_data.end(); ConstLangIterator++)
+	{
+		currentXMLData = listXMLDatas->InsertNewChildElement("Data");
+
+		if (currentXMLData == nullptr)
+			continue;
+
+		currentXMLData->SetAttribute("Id", ConstLangIterator->first.c_str());
+		currentXMLData->SetText(ConstLangIterator->second.c_str());
+	}
+
+	currentLanguageXMLDocument.SaveFile((m_path + m_languages[m_currentLanguage].first + ".xml").c_str());
 }
 
 void Core::LocalisationsManager::ShowEditorControl()
 {
+	if (ImGui::BeginCombo("##comboLanguageSelection", (m_languages[m_currentLanguage].first + " - " + m_languages[m_currentLanguage].second).c_str()))
+	{
+		for (size_t n = 0; n < m_languages.size(); n++)
+		{
+			const bool is_selected = (m_currentLanguage == n);
+			if (ImGui::Selectable((m_languages[n].first + " - " + m_languages[n].second).c_str(), is_selected))
+				SetLanguage(n);
+
+			// Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
+			if (is_selected)
+				ImGui::SetItemDefaultFocus();
+		}
+		ImGui::EndCombo();
+	}
+
+	ImGui::SameLine();
+
+	static std::string neLanguageId = "";
+	static std::string neLanguageName = "";
+
+	if (ImGui::Button("Add language"))
+	{
+		neLanguageId = "";
+		neLanguageName = "";
+		ImGui::OpenPopup("New Language");
+	}
+
+	// Always center this window when appearing
+	ImVec2 center = ImGui::GetMainViewport()->GetCenter();
+	ImGui::SetNextWindowPos(center, ImGuiCond_Appearing, ImVec2(0.5f, 0.5f));
+	if (ImGui::BeginPopupModal("New Language", NULL, ImGuiWindowFlags_AlwaysAutoResize))
+	{
+		ImGui::InputText("New language id", &neLanguageId);
+		ImGui::InputText("New language name", &neLanguageName);
+
+		ImGui::Separator();
+
+		if (ImGui::Button("OK", ImVec2(120, 0)))
+		{
+			ImGui::CloseCurrentPopup();
+			m_languages.push_back({ neLanguageId, neLanguageName });
+		}
+
+		ImGui::SetItemDefaultFocus();
+		ImGui::SameLine();
+		if (ImGui::Button("Cancel", ImVec2(120, 0))) { ImGui::CloseCurrentPopup(); }
+		ImGui::EndPopup();
+	}
+
+	ImGui::SameLine();
+
+	if (ImGui::Button("Save Language Data"))
+		SaveCurrentLanguage();
+
+	ImGui::Separator();
+
+	ImGui::BeginChild("scrollInData");
+
+	if (ImGui::BeginTable("LanguageDataTable", 3, ImGuiTableFlags_Resizable | ImGuiTableFlags_RowBg | ImGuiTableFlags_Borders))
+	{
+		ImGui::TableSetupColumn("Key");
+		ImGui::TableSetupColumn("Value");
+		ImGui::TableHeadersRow();
+
+		std::vector<std::string> ToDelete;
+
+		for (std::unordered_map<std::string, std::string>::iterator row = m_data.begin(); row != m_data.end(); row++)
+		{
+			ImGui::TableNextRow();
+			ImGui::TableSetColumnIndex(0);
+			ImGui::Text(row->first.c_str());
+
+			ImGui::TableSetColumnIndex(1);
+			ImGui::SetNextItemWidth(ImGui::GetColumnWidth(1));
+			ImGui::InputText(("##ValueOf" + row->first).c_str(), &(row->second));
+
+			ImGui::TableSetColumnIndex(2);
+			if (ImGui::Button(("Delete##DeleteOf" + row->first).c_str()))
+				ToDelete.push_back(row->first);
+		}
+
+		for (std::vector<std::string>::const_iterator iterToDel = ToDelete.begin(); iterToDel != ToDelete.end(); iterToDel++)
+			m_data.erase(*iterToDel);
+
+		static std::string LanguageKeyToAdd = "";
+
+		ImGui::TableNextRow();
+		ImGui::TableSetColumnIndex(0);
+		ImGui::SetNextItemWidth(ImGui::GetColumnWidth(0));
+		ImGui::InputText("##KeyDataToAdd", &LanguageKeyToAdd);
+
+		ImGui::TableSetColumnIndex(1);
+		if (ImGui::Button("Add"))
+		{
+			if (LanguageKeyToAdd == "" || m_data.find(LanguageKeyToAdd) != m_data.end())
+				R3DE_WARNING("Can't add empty or existing Key on data");
+			else
+				m_data.insert({ LanguageKeyToAdd, std::string("") });
+
+			LanguageKeyToAdd = "";
+		}
+
+		ImGui::EndTable();
+	}
+
+	ImGui::EndChild();
 }
 #endif // _EDITOR
